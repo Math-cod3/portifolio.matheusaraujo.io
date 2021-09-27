3ac4833f0ee62c35b6bbcd0925ad559a53f885bb
Matheus de Araujo de Souza <79392166+Math-cod3@users.noreply.github.com>
2021-09-27 13:40:33 -0300

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -41,6 +41,7 @@ const target = document.querySelectorAll('[data-anime]');
 const animationClass = 'animate';
 
 function animeScroll() {
+  console.log("ativou")
   const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
   target.forEach(function(element) {
     if((windowTop) > element.offsetTop) {
